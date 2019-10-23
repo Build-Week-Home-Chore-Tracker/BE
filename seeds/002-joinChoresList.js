547cd564efb567ecff1cc28d674423333c4f8779
Joshua-Shockley <thelumbashockley@gmail.com>
2019-10-23 13:34:33 -0500

    made adjustments to seed to match new users better with original seed data

--- seeds/002-joinChoresList.js
+++ seeds/002-joinChoresList.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
             // Inserts seed entries
             return knex('choreList').insert([
                 { userId: 2, choreId: 12, notes: 'blah', dateMade: '', bonusP: 1, completed: false },
-                { userId: 6, choreId: 22, notes: 'blah', dateMade: '', bonusP: 2, completed: false },
-                { userId: 8, choreId: 36, notes: 'blah', dateMade: '', bonusP: 0, completed: false },
-                { userId: 6, choreId: 12, notes: 'blah', dateMade: '', bonusP: 0, completed: false },
+                { userId: 2, choreId: 22, notes: 'blah', dateMade: '', bonusP: 2, completed: false },
+                { userId: 3, choreId: 36, notes: 'blah', dateMade: '', bonusP: 0, completed: false },
+                { userId: 3, choreId: 12, notes: 'blah', dateMade: '', bonusP: 0, completed: false },
 
             ]);
         });
